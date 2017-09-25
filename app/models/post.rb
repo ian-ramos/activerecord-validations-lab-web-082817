c508dd30c96e23bf4d20faec069b260a94363566
ian-ramos <ramos.ianc@gmail.com>
2017-09-25 11:10:16 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+  validate :non_clickbait
+
+  def non_clickbait
+    array = ["Won't Believe", "Secret", "Guess"]
+    if self.title
+      array.each do |string|
+        if self.title.include?(string)
+          return # if title includes phrase, then it's fine
+        end
+      end
+      self.errors[:title] << "Not click baity enough."
+    end
+  end
+
 end
